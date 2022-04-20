a5309e1b325cc495730f4b6488d48a78d4c4d0c1
Amrutha Krishna <amruthakrishna.unni@gmail.com>
2022-04-20 13:17:55 +0200

    Untagged-tag diphoton dumper

---
+++ Dumpers/dump_signal_UL17.sh
@@ -0,0 +1,13 @@
+nEvents=-1
+outdir=UntaggedTag_dump_hggInt_UL17_sigMC
+dumper=$CMSSW_BASE/src/flashgg/Dumpers/diphoton_dumper.py
+queue=tomorrow
+json=$CMSSW_BASE/src/flashgg/Dumpers/hgg_width_UL17_sigMC.json
+
+fggRunJobs.py  \
+    --load $json \
+    -d $outdir \
+    -q $queue \
+    -n 94 \
+    --no-copy-proxy -D -P \
+    -x cmsRun $dumper maxEvents=$nEvents copyInputMicroAOD=False
