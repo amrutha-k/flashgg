0ad17795dd888241067043e4bfb0433012cd73c3
Amrutha Krishna <amruthakrishna.unni@gmail.com>
2024-04-29 11:43:16 +0200

    fixes to the ntuplizer json

--- Dumpers/dump_int_UL16preVFP.sh
+++ Dumpers/dump_int_UL16preVFP.sh
@@ -1,12 +1,12 @@
 nEvents=-1
-outdir=intSummer20UL16preVFPnoCats
+outdir=intggSummer20UL16preVFPnoCats
 dumper=$CMSSW_BASE/src/flashgg/Dumpers/workspaceStd.py
 queue=tomorrow
-json=$CMSSW_BASE/src/flashgg/Dumpers/UL16preVFP_intMC.json
+json=$CMSSW_BASE/src/flashgg/Dumpers/UL16preVFP_intgg.json
 
 fggRunJobs.py  \
     --load $json \
-    --stage-to /eos/user/a/amkrishn/hggWidth/mcNtuples/condor_output/2016preVFP/intSummer20UL16preVFPnoCats \
+    --stage-to /eos/user/a/amkrishn/hggWidth/mcNtuples/condor_output/2016preVFP/intggSummer20UL16preVFPnoCats \
     -d $outdir \
     -q $queue \
     -n 100 \
